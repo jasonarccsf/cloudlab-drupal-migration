9ac11a5b551f96754ed657ce45dd83c7290dea60
Jason Riddle <jason.riddle@sfgov.org>
2023-12-14 09:11:31 -0800

    Add templates

--- infrastructure/aws/accounts/sfgov-tis-sandbox/main.tf
+++ infrastructure/aws/accounts/sfgov-tis-sandbox/main.tf
@@ -18,3 +18,24 @@ resource "aws_lightsail_instance" "drupal7_dpa" {
 output "drupal7_dpa_public_ip_address" {
   value = aws_lightsail_instance.drupal7_dpa.public_ip_address
 }
+
+# TODO: This will just be a repeated template to copy from.
+
+# resource "aws_lightsail_instance" "drupal7_foobar" {
+#   name              = "drupal7-foobar.ec2"
+#   availability_zone = data.aws_availability_zones.available.names[0]
+#   blueprint_id      = "ubuntu_22_04"
+#   bundle_id         = "medium_3_0"
+
+#   key_pair_name = "id_rsa.drupal"
+
+#   tags = {
+#     SiteName                   = "FOOBAR"
+#     SiteDrupalVersion          = "7.99"
+#     SiteDrupalMajorVersionOnly = "7"
+#   }
+# }
+
+# output "drupal7_foobar_public_ip_address" {
+#   value = aws_lightsail_instance.drupal7_foobar.public_ip_address
+# }
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,3 +7,8 @@ module "aws" {
 output "drupal7_dpa_public_ip_address" {
   value = module.aws.drupal7_dpa_public_ip_address
 }
+
+# TODO: This will just be a repeated template to copy from.
+# output "drupal7_foobar_public_ip_address" {
+#   value = module.aws.drupal7_foobar_public_ip_address
+# }
