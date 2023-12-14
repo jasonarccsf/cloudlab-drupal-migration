902deff9e4f38c51f69c86865f0d04e06b343487
Jason Riddle <jason.riddle@sfgov.org>
2023-12-14 08:50:21 -0800

    Refer to vars

--- infrastructure/aws/accounts/sfgov-tis-sandbox/main.tf
+++ infrastructure/aws/accounts/sfgov-tis-sandbox/main.tf
@@ -1,10 +1,20 @@
 data "aws_availability_zones" "available" {}
 
-resource "aws_lightsail_instance" "drupal7" {
-  name              = "drupal7.ec2"
+resource "aws_lightsail_instance" "drupal7_dpa" {
+  name              = "drupal7-dpa.ec2"
   availability_zone = data.aws_availability_zones.available.names[0]
   blueprint_id      = "ubuntu_22_04"
   bundle_id         = "medium_3_0"
 
   key_pair_name = "id_rsa.drupal"
+
+  tags = {
+    SiteName                   = "DPA"
+    SiteDrupalVersion          = "7.99"
+    SiteDrupalMajorVersionOnly = "7"
+  }
+}
+
+output "drupal7_dpa_public_ip_address" {
+  value = aws_lightsail_instance.drupal7_dpa.public_ip_address
 }
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,3 +3,7 @@
 module "aws" {
   source = "./aws/accounts/sfgov-tis-sandbox"
 }
+
+output "drupal7_dpa_public_ip_address" {
+  value = module.aws.drupal7_dpa_public_ip_address
+}
