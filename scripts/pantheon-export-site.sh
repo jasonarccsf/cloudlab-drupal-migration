fdadf99b7d53429c8ba2aefc04de31e672931f34
Jason Riddle <jason.riddle@sfgov.org>
2023-12-14 08:53:55 -0800

    Add quotes

--- scripts/pantheon-export-site.sh
+++ scripts/pantheon-export-site.sh
@@ -9,12 +9,12 @@ terminus --version
 
 # Export the database to $HOME/pantheon-local-copies/db/${SITE}-db.tgz.
 # See: https://docs.pantheon.io/terminus/commands/local-getLiveDB
-terminus local:getLiveDB --no-interaction --overwrite -- ${SITE}
+terminus local:getLiveDB --no-interaction --overwrite -- "${SITE}"
 
 # Export the files to $HOME/pantheon-local-copies/files/${SITE}-files.tgz.
 # See: https://docs.pantheon.io/terminus/commands/local-getLiveFiles
-terminus local:getLiveFiles --no-interaction --overwrite -- ${SITE}
+terminus local:getLiveFiles --no-interaction --overwrite -- "${SITE}"
 
 # Export the site to $HOME/pantheon-local-copies/${SITE}.
 # See: https://docs.pantheon.io/terminus/commands/local-clone
-terminus local:clone --no-interaction --override -- ${SITE}
+terminus local:clone --no-interaction --override -- "${SITE}"
